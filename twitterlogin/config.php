eb64aac00fe71f4b81baad19c4b9d5c372ca6a53
agnes <36883923+agnesba@users.noreply.github.com>
2018-04-11 22:22:43 +0100

    Callback
    Changedcallback

--- twitterlogin/config.php
+++ twitterlogin/config.php
@@ -1,5 +1,5 @@
 <?php
 define('CONSUMER_KEY', 'xSTSxHCYAzLJeSUtgkMR56WDn');
 define('CONSUMER_SECRET', 'qUiQs6SrxsU5jMSzeZZMIYJIewmWLZfKWUudnVOguH2FNKqK01');
-define('OAUTH_CALLBACK', 'https://dreamy-lewin-8f6f5a.netlify.com/home');
+define('OAUTH_CALLBACK', 'https://dreamy-lewin-8f6f5a.netlify.com/twitterlogin/process.php');
 ?>
